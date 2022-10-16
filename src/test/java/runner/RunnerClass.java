707036c116fdefe166ed2ae85b3d73d3b7d47db0
Arundhaty <arundhaty888@gmail.com>
2022-10-16 17:23:30 -0500

    committed

---
+++ src/test/java/runner/RunnerClass.java
@@ -0,0 +1,28 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features= "src\\test\\java\\feature",
+	glue="steps",
+	tags="@Regression",
+//	tags="@ChangeToSkyBlue",
+//	tags="@ChangeToWhite",
+	
+	monochrome=true,
+	//dryRun = false,
+	plugin= {
+			"pretty",
+			"html:target/cucumber",
+			"json:target/cucumber.json"
+	}
+	
+)
+
+
+public class RunnerClass {
+}
